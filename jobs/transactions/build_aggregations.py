fb3ba02cc1dbe99852d785fef0d957ab63ed29c4
parinibrahma <parinibrahma@parinibahmasMBP.lan1>
2019-03-09 16:05:49 -0500

    pushing more tweaks

--- jobs/transactions/build_aggregations.py
+++ jobs/transactions/build_aggregations.py
@@ -11,7 +11,7 @@ import pyspark.sql.functions as F
 from datetime import datetime
 from pyspark.sql.functions import col, udf
 from pyspark.sql.types import DateType
-
+import calendar
 
 def get_aggregations(year, month):
     """
@@ -23,24 +23,16 @@ def get_aggregations(year, month):
     category_flags_path = config["lookup"]["category_flags"]
     desc_flags_path = config["lookup"]["description_flags"]
 
-    func = udf(lambda x: datetime.strptime(x, '%m/%d/%Y'), DateType())
+    start_date = str(year)+month+'01'
+    _, end_day = calendar.monthrange(year, int(month))
+    end_date = str(year)+month+str(end_day)
 
-    #start_date = str(year)+'-' + str(month)+'-01'
-    #end_date = str(year)+'-' + str(month)+'-31'
-
-    start_date = '2017-07-01'
-    end_date = '2017-07-31'
-
-    monthly_transactions = spark.read.csv(all_transactions_path+'/*/*.csv', sep=',', header=True)
-    #all_transactions = all_transactions.withColumn("Transaction_date", func(col('Transaction_date')))
-    #monthly_transactions = all_transactions.filter(f"Transaction_date between {start_date} and {end_date}")
+    all_transactions = spark.read.csv(all_transactions_path+'/*/*.csv', sep=',', header=True)
+    monthly_transactions = all_transactions.filter(f"trndt between {start_date} and {end_date}")
     category_flags = spark.read.csv(category_flags_path, sep=',', header=True)
     desc_flags = spark.read.csv(desc_flags_path, sep=',', header=True)
 
     monthly_transactions.show()
-    monthly_transactions.printSchema()
-    category_flags.show()
-    desc_flags.show()
 
     desc_only_flags = desc_flags.select('DESCRIPTIONS')
     Flag_df = [str(i.DESCRIPTIONS) for i in desc_only_flags.collect()]
@@ -61,19 +53,25 @@ def get_aggregations(year, month):
 
     transactions = t.join(f, t.new_Description == f.DESCRIPTIONS, "left_outer").drop("DESCRIPTIONS")
 
+    transactions = transactions.withColumn("Amount", replace_comma("Amount"))
+
     transactions.show(20, False)
 
-    null_flags = transactions.filter("FLAG is NULL")
-    null_flags.show(100, False)
+    transactions.groupby('act_type').agg(F.count("act_type").alias("total_act_type_transactions")).show()
+
+    null_flags = transactions.filter("FLAG = 'FD'")
+    null_flags.orderBy(F.asc("trndt")).show(100, False)
 
     grouped_df = transactions.groupby('FLAG').agg(F.sum("Amount").alias("total_amt"))
     print('grouped_df')
     grouped_df.join(category_flags, "FLAG", "left_outer")\
-        .orderBy(F.desc("total_amt")).show(200)
+        .orderBy(F.desc("total_amt")).show(200, False)
 
     pass
 
 
-get_aggregations(year=2018, month='08')
+replace_comma = F.udf(lambda s: s.replace(",", ""))
+
+get_aggregations(year=2018, month='09')
 
 
--- jobs/transactions/build_all_transactions.py
+++ jobs/transactions/build_all_transactions.py
@@ -6,6 +6,8 @@ fs = spark._jvm.org.apache.hadoop.fs.FileSystem.get(spark._jsc.hadoopConfigurati
 from resources.config import app_config as config
 import datetime
 import pyspark.sql.functions as F
+import pyspark.sql.types as t
+
 
 def run_all_transactions(spark, config):
     """
@@ -32,10 +34,23 @@ def run_all_transactions(spark, config):
 
     master_header = ['Transaction_date', 'Description', 'Amount', 'trndt', 'act_type']
 
-    bofa_cc_master_df = spark.read.csv(bofa_cc_master, header=True, sep=',').select(*bofacredit)
+
+
+
     bofa_chk_master = spark.read.csv(bofa_chk_master, header=True, sep=',').select(*bofachk)
     discover_master = spark.read.csv(discover_master, header=True, sep=',').select(*discover)
     citi_master = spark.read.csv(citi_master, header=True, sep=',').select(*citi)
+    bofa_cc_master_df = spark.read.csv(bofa_cc_master, header=True, sep=',').select(*bofacredit)
+
+    citi_master = citi_master.\
+        withColumn('Amount', replace_lp("Amount")).\
+        withColumn('Amount', replace_rp("Amount")).\
+        withColumn('Amount', replace_dol("Amount"))
+
+    discover_master = discover_master.withColumn("Amount", flip_sign("Amount"))
+    bofa_cc_master_df = bofa_cc_master_df.withColumn("Amount", flip_sign("Amount"))
+    citi_master = citi_master.withColumn("Amount", flip_sign("Amount"))
+
 
     master_df = bofa_cc_master_df.\
         union(bofa_chk_master).\
@@ -44,9 +59,22 @@ def run_all_transactions(spark, config):
 
     master_df = master_df.withColumn('Description', F.upper(F.col('Description')))
 
+    master_df.filter("Description like '%PAYMENT%'").show(100, False)
+
+
     master_df.coalesce(1).write.format("csv").mode("overwrite").save(all_master_path, header="true")
 
     pass
+
+
+def flip_sign(column):
+    return F.when(F.col(column).isNotNull(), -F.col(column)).otherwise(0)
+
+
+replace_lp = F.udf(lambda s: s.replace("(", "-"))
+replace_rp = F.udf(lambda s: s.replace(")", ""))
+replace_dol = F.udf(lambda s: s.replace("$", ""))
+
 
 run_all_transactions(spark, config)
 
